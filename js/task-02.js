15690e61822f023e7e05b6e6a60532805ecb6085
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-03-01 20:57:13 +0200

    result03

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments"
 ];
+
+const navEl = document.querySelector("#ingredients");
+
+const navItemEl = document.createElement("li");
+
+navItemEl.textContent = ingredients;
+navItemEl.classList.add("item");
+
+navEl.appendChild(navItemEl);
+
+console.log(navItemEl);
+console.log(navEl);
