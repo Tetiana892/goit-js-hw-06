c3e30879ce86946f3ebf3ddb474fb041ecf22fb9
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-03-05 17:09:26 +0200

    task10_result

--- js/task-10.js
+++ js/task-10.js
@@ -6,23 +6,37 @@ function getRandomHexColor() {
 
 // Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
 // Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
 
 // Размеры самого первого <div> - 30px на 30px.
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 // Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxesEl = document.getElementById("boxes");
+
+btnCreate.addEventListener("click", getAmount);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  const amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const size = 30 + i * 10;
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.background = getRandomHexColor();
+
+    boxesEl.appendChild(div);
+  }
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
